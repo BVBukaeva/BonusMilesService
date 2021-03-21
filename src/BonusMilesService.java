0b9bb83a5a8fd4340403c33eb7f218653c9e384a
Bukaeva Bayrta <bbembeeva@gmail.com>
2021-03-21 23:10:43 +0300

    отформатировала код

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,6 +1,6 @@
 public class BonusMilesService {
     public int calculate(int cost) {
-        int miles_amount = cost / 20;
-        return miles_amount;
+        int milesAmount = cost / 20;
+        return milesAmount;
     }
 }
